d508439f798f3538085184803c962d3e14fee6a6
RanDoMuy <100051932+RanDoMuy@users.noreply.github.com>
2024-05-06 14:20:52 -0700

    Update settings.py

--- WealthOrigin/settings.py
+++ WealthOrigin/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o*v@utr_ld$)l387zw0qza(^xy1_1ogek!^^$81=ka276xu*r('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
