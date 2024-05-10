9f9a9c8baac39914746bb876508472dcf4a7dacd
RanDoMuy <100051932+RanDoMuy@users.noreply.github.com>
2024-05-10 18:10:57 +0100

    Update apps.py

--- wealth_origin_app/apps.py
+++ wealth_origin_app/apps.py
@@ -5,7 +5,7 @@ class WealthOriginAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wealth_origin_app'
 
-    def ready(self):
-        from . import updater
-        updater.start()
+    #def ready(self):
+        #from . import updater
+        #updater.start()
 
