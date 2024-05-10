e09483d7c35d4313f19d8c909a3133ea52ef3e90
RanDoMuy <100051932+RanDoMuy@users.noreply.github.com>
2024-05-10 18:10:14 +0100

    Update updater.py

--- wealth_origin_app/updater.py
+++ wealth_origin_app/updater.py
@@ -1,7 +1,7 @@
-from .task import increase_profit_balance
-from apscheduler.schedulers.background import BackgroundScheduler
+#from .task import increase_profit_balance
+#from apscheduler.schedulers.background import BackgroundScheduler
 
-def start():
-    scheduler= BackgroundScheduler()
-    scheduler.add_job(increase_profit_balance, 'interval', seconds=7200)
-    scheduler.start()
+#def start():
+    #scheduler= BackgroundScheduler()
+    #scheduler.add_job(increase_profit_balance, 'interval', seconds=7200)
+    #scheduler.start()
